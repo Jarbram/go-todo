2f163cba25e182dd8213c3540c1ecb89c40aff06
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-02-01 13:16:56 -0500

    add the server function and todo group function

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -36,8 +40,45 @@ type (
 func init() {
 	rnd = renderer.New()
 	session, err := mgo.Dial(hostName)
+	CheckErr(err)
+	db = session.DB(dbName)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/todo", todoHandler())
+
+	svr := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		log.Println("listening on port", port)
+		if err := svr.ListenAndServe(); err != nil {
+			log.Printf("listen:%s\n", err)
+		}
+	}()
+}
+
+func todoHandler() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", getTodos)
+		r.Post("/", createTodoHandler)
+		r.Put("/{id}", updateTodoHandler)
+		r.Delete("/{id}", deleteTodoHandler)
+	})
+	return rg
+}
+
+func CheckErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	db = session.DB(dbName)
 }
